b656e17d1afe43a0a1925480228bc0ced5c1d625
HuangJunLong <297953766@qq.com>
2019-01-30 15:26:46 +0800

    添加方法

--- app/src/main/java/com/example/newplanewar/TestBranch.java
+++ app/src/main/java/com/example/newplanewar/TestBranch.java
@@ -2,4 +2,11 @@ package com.example.newplanewar;
 
 public class TestBranch {
     public int test = 0;
+    public TestBranch(){
+
+    }
+
+    public int getTest(){
+        return test;
+    }
 }
